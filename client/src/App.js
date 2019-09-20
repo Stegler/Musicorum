2ff6aab2ed587a495b4caaed399367d70211c91f
Ramon Vidallon <ramon.vidallon@icloud.com>
2019-09-19 22:51:18 -0700

    created the routes/page for the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import StartPage from './pages/startPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Switch>
+            <Route exact path='/' component={StartPage}></Route>
+            {/* <Route exact path='#' component={}></Route>
+            <Route exact path='#' component={}></Route>
+            <Route exact path='#' component={}></Route>
+            <Route exact path='#' component={}></Route> */}
+      </Switch>
+
     </div>
+    </Router>
   );
 }
 
